4d4883a963204838d7df3338261ec40930891206
nathansdalton <131366784+nathansdalton@users.noreply.github.com>
2024-09-12 17:46:26 +1000

    button change

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -31,7 +31,7 @@ app.layout = dbc.Container([
             html.Div([
                 dbc.Button("Watch on Twitch", color="dark", className="mb-3 w-100", href="https://www.twitch.tv/pian8"),
                 dbc.Button("Donate", color="dark", className="mb-3 w-100", href="https://www.justgiving.com/page/pian8"),
-                dbc.Button("Song List", color="dark", href="https://www.streamersonglist.com/t/pian8/songs"),
+                dbc.Button("Song List", color="dark",  className="w-100", href="https://www.streamersonglist.com/t/pian8/songs"),
             ], className="d-flex flex-column align-items-center", style={"max-width": "200px", "margin": "0 auto"})
         ], width=8, className="mx-auto")
     ])
